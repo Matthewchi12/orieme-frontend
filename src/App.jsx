cec22cf22695ac20aee801732a68474774ed9aa9
Matthewchi12 <matthewchi1221@gmail.com>
2026-05-19 12:18:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,4 @@
- export default function App() {
-  return (
-    <div style={{
-      background: "black",
-      color: "white",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      flexDirection: "column"
-    }}>
-      <h1 style={{ color: "#D4AF37", fontSize: "40px" }}>
-        ORIEME MATTHEW FASHION STORE
-      </h1>
-      <p>Your brand is now live 🚀</p>
-    </div>
-  )
+
 }
 import { useState } from "react";
 
