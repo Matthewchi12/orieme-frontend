c48aa3456c5f0e1e65cd1b44a0c681427ecd2f0a
Matthewchi12 <matthewchi1221@gmail.com>
2026-05-19 11:32:02 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div style={{
+      background: "black",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column"
+    }}>
+      <h1 style={{ color: "#D4AF37" }}>
+        Orieme Fashion Store
+      </h1>
+      <p>Frontend is working 🚀</p>
+    </div>
+  )
+}
